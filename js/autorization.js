39ab27214947ba8b96ffb5ce97599b04ba062ba1
znewk <zane.css34@gmail.com>
2021-05-06 19:49:49 +0600

    autorization system, users data added

---
+++ js/autorization.js
@@ -0,0 +1,132 @@
+let changeToReg = document.getElementsByClassName('changeButton')[0]
+let changeToLog = document.getElementsByClassName('changeButton')[1]
+let username = document.getElementById("usernameInput")
+let login = document.getElementById("loginInput")
+let password = document.getElementById("passwordInput")
+let passwordRepeat = document.getElementById("repeatInput")
+let users = JSON.parse(localStorage.getItem("users"))
+
+changeToReg.addEventListener('click', function() {
+    document.title = 'RIQZO Registration'
+    changeToLog.classList.remove('active')
+    changeToReg.classList.add('active')
+    $('#content').empty()
+    $('#content').append(`
+    <div class="inputBody">
+                    <img src="./img/user.png" alt="" class="inputIcon">
+                    <input type="text" class="input" placeholder="Введите ваше имя" id="usernameInput">
+                </div>
+
+                <div class="inputBody">
+                    <img src="./img/user.png" alt="" class="inputIcon">
+                    <input type="text" class="input" placeholder="Придумайте логин" id="loginInput">
+                </div>
+                <div class="passInputs">
+                    <div class="inputBody">
+                        <img src="./img/password.png" alt="" class="inputIcon">
+                        <input type="password" class="input passInput" placeholder="Придумайте пароль" id="passwordInput">
+                    </div>
+                    <div class="inputBody">
+                        <img src="./img/password.png" alt="" class="inputIcon">
+                        <input type="password" class="input passInput" placeholder="Повторите пароль" id="repeatInput">
+                    </div>
+                </div>
+                <div class="flex">
+                    <button class="orangeButton" onclick="register()">Зарегистрироваться</button>
+                </div>
+    `)
+})
+changeToLog.addEventListener('click', function() {
+    document.title = 'RIQZO Log In'
+    changeToReg.classList.remove('active')
+    changeToLog.classList.add('active')
+    $('#content').empty()
+    $('#content').append(`
+    <div class="inputBody">
+                    <img src="./img/user.png" alt="" class="inputIcon">
+                    <input type="text" class="input" placeholder="Введите логин" id="loginInput">
+                </div>
+
+                <div class="inputBody">
+                    <img src="./img/password.png" alt="" class="inputIcon">
+                    <input type="password" class="input" placeholder="Введите пароль" id="passwordInput">
+                </div>
+                <div class="flex">
+                    <button class="orangeButton" onclick="loginBtn()">Войти</button>
+                </div>
+    `)
+})
+
+let register = () => {
+    if (username.value.length < 6) {
+        username.style.border = "2px solid red"
+        alert('Слишком короткое имя!')
+    } else if (login.value.length < 6) {
+        login.style.border = "2px solid red"
+        alert('Логин должен состоять как минимум из 6 знаков')
+    } else if (password.value.length < 8 && passwordRepeat.value.length < 8) {
+        password.style.border = "2px solid red"
+        passwordRepeat.style.border = "2px solid red"
+        alert('Пароль должен состоять как минимум из 8 знаков')
+    } else if (password.value !== passwordRepeat.value) {
+        password.style.border = "2px solid red"
+        passwordRepeat.style.border = "2px solid red"
+        alert('Пароли не совпадают')
+    } else {
+        let random = Math.floor(Math.random() * 1000) + 1;
+        let user = {
+            userId: random,
+            userName: username.value,
+            userLogin: login.value,
+            userPassword: password.value,
+        }
+        users.push(user)
+        localStorage.setItem('users', JSON.stringify(users))
+        alert('Вы успешно зарегистрировались!')
+        document.title = 'RIQZO Log In'
+        changeToReg.classList.remove('active')
+        changeToLog.classList.add('active')
+        $('#content').empty()
+        $('#content').append(`
+        <div class="inputBody">
+        <img src="./img/user.png" alt="" class="inputIcon">
+        <input type="text" class="input" placeholder="Введите логин" id="loginInput">
+    </div>
+
+    <div class="inputBody">
+        <img src="./img/password.png" alt="" class="inputIcon">
+        <input type="password" class="input" placeholder="Введите пароль" id="passwordInput">
+    </div>
+    <div class="flex">
+        <button class="orangeButton" onclick="loginBtn()">Войти</button>
+    </div>
+    `)
+    }
+}
+
+
+
+
+console.log(users)
+
+let loginBtn = () => {
+    let loginLog = document.getElementById("loginInput")
+    let passwordLog = document.getElementById("passwordInput")
+    for (i = 0; i < users.length; i++) {
+        if (loginLog.value === users[i].userLogin && passwordLog.value === users[i].userPassword) {
+            let loggedUser = {
+                userId: users[i].userId,
+                userName: users[i].userName,
+                userLogin: users[i].userLogin,
+                userPassword: users[i].userPassword,
+            }
+            localStorage.setItem('loggedUser', JSON.stringify(loggedUser))
+            location.href = 'index.html'
+        } else {
+            loginLog.style.border = '2px solid red'
+            passwordLog.style.border = '2px solid red'
+            alert('Неверные данные!')
+        }
+
+    }
+}
--- js/header.js
+++ js/header.js
@@ -1,7 +1,7 @@
 var header = $('.header'),
     scrollPrev = 0;
 
-$(window).scroll(function () {
+$(window).scroll(function() {
     var scrolled = $(window).scrollTop();
 
     if (scrolled > 100 && scrolled > scrollPrev) {
@@ -11,3 +11,15 @@ $(window).scroll(function () {
     }
     scrollPrev = scrolled;
 });
+let profile = document.getElementById('profile')
+if (localStorage.getItem('loggedUser') === null) {
+    profile.innerText = 'Войти'
+    profile.addEventListener('click', function() {
+        location.href = 'autorization.html'
+    })
+} else {
+    let loggedUser = JSON.parse(localStorage.getItem('loggedUser'))
+    console.log(loggedUser)
+    profile.innerText = 'Личный кабинет'
+    profile.removeEventListener('click')
+}
